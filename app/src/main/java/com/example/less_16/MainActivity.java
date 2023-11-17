a761c03c3639ccea8913d08b9147a6e60a4b2d11
Golobon <golobon@ya.ru>
2023-11-17 13:24:42 +0300

    +

--- app/src/main/java/com/example/less_16/MainActivity.java
+++ app/src/main/java/com/example/less_16/MainActivity.java
@@ -7,12 +7,15 @@ import androidx.core.view.MenuProvider;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.VibrationAttributes;
+import android.view.ContextMenu;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -25,68 +28,41 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener, MenuProvider {
-    EditText eT1;
-    EditText eT2;
-    Button b1;
-    Button b2;
-    Button b3;
-    Button b4;
-    TextView tW;
-    final int MENU_RESET = 1;
-    final int MENU_EXIT = 2;
+public class MainActivity extends AppCompatActivity {
+    TextView tV;
+    final int ALPHA = 1;
+    final int ROTATE = 2;
+    final int SCALE = 3;
+    final int TRANS = 4;
+    final int COMBO = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        eT1 = findViewById(R.id.editTextText);
-        eT2 = findViewById(R.id.editTextText2);
-
-        b1 = findViewById(R.id.button);
-        b1.setOnClickListener(this);
-        b2 = findViewById(R.id.button2);
-        b2.setOnClickListener(this);
-        b3 = findViewById(R.id.button3);
-        b3.setOnClickListener(this);
-        b4 = findViewById(R.id.button4);
-        b4.setOnClickListener(this);
-
-        tW = findViewById(R.id.textView);
-        tW.setTextSize(26);
-
-        addMenuProvider(this);
+        tV = findViewById(R.id.tV);
+        registerForContextMenu(tV);
+    }
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        menu.add(0, ALPHA, 0, "Alpha");
+        menu.add(0, ROTATE, 0, "Rotate");
+        menu.add(0, SCALE, 0, "Scale");
+        menu.add(0, TRANS, 0, "Trans");
+        menu.add(0, COMBO, 0, "Combo");
     }
 
     @Override
-    public void onClick(View v) {
-        if (!eT1.getText().toString().equals("") && !eT2.getText().toString().equals("")) {
-            float val1 = Float.parseFloat(eT1.getText().toString());
-            float val2 = Float.parseFloat(eT2.getText().toString());
-            if (v.equals(b1)) tW.setText(String.valueOf(val1 + val2));
-            if (v.equals(b2)) tW.setText(String.valueOf(val1 - val2));
-            if (v.equals(b3)) tW.setText(String.valueOf(val1 * val2));
-            if (v.equals(b4)) {
-                if (val2 != 0) tW.setText(String.valueOf(val1 / val2));
-                else Toast.makeText(this, "На ноль делить нельзя", Toast.LENGTH_LONG).show();
-            }
-        } else Toast.makeText(this, "Не заполнены все поля", Toast.LENGTH_LONG).show();
-    }
-
-    @Override
-    public void onCreateMenu(@NonNull Menu menu, @NonNull MenuInflater menuInflater) {
-        menu.add(0, MENU_RESET, 0, "Reset");
-        menu.add(0, MENU_EXIT, 0, "Exit");
-    }
-
-    @Override
-    public boolean onMenuItemSelected(@NonNull MenuItem menuItem) {
-        int res = menuItem.getItemId();
-        switch (res) {
-            case MENU_RESET : eT1.setText(""); eT2.setText(""); tW.setText(""); break;
-            case MENU_EXIT : finish(); break;
-        }
-        return true;
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        int res = item.getItemId();
+        Animation anim = null;
+        if (res == ALPHA) anim = AnimationUtils.loadAnimation(this, R.anim.alpha);
+        if (res == ROTATE) anim = AnimationUtils.loadAnimation(this, R.anim.rotate);
+        if (res == SCALE) anim = AnimationUtils.loadAnimation(this, R.anim.scale);
+        if (res == TRANS) anim = AnimationUtils.loadAnimation(this, R.anim.trans);
+        if (res == COMBO) anim = AnimationUtils.loadAnimation(this, R.anim.combo);
+        tV.startAnimation(anim);
+        return super.onContextItemSelected(item);
     }
 }
